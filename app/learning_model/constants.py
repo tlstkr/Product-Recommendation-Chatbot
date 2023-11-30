420ae0937e001dac12f20812be422557f39c42b4
tlstkr <tlstkr@icloud.com>
2023-11-30 02:44:45 +0000

    add constants.py

---
+++ app/learning_model/constants.py
@@ -0,0 +1,15 @@
+# FILL CUSTOM AZURE_OPENAI_API_KEY
+AZURE_OPENAI_API_KEY = ''
+
+IMAGES_PATH = 'app/datasets/images/'
+OPENAI_API_TYPE = 'azure'
+OPENAI_API_VERSION = '2023-07-01-preview'
+AZURE_OPENAI_ENDPOINT = 'https://dg-openai-api.openai.azure.com'
+GPT_ENGINE = 'azure-gpt-35-turbo'
+TEXT_EMBEDDING_ENGINE = 'azure-text-embedding-ada-002'
+BLIP_IMAGE_TO_TEXT_MODEL = 'Salesforce/blip-image-captioning-large'
+IMAGE_TO_TEXT_TASK = 'image-to-text'
+CLIP_SENTENCE_TRANSFORMER = 'clip-ViT-B-32'
+STABLE_DIFFUSOR = 'stabilityai/stable-diffusion-2'
+VQA_MODEL = "Salesforce/blip-vqa-capfilt-large"
+VQA_TASK = "visual-question-answering"
